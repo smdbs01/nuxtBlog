87f0feb97dafd142b1fed4dd1dbb80b23c8483e6
smdbs <1289707349@qq.com>
2024-02-16 18:20:20 -0500

    Switched to pool connection

--- server/db/index.ts
+++ server/db/index.ts
@@ -2,11 +2,11 @@ import { drizzle } from "drizzle-orm/mysql2";
 import * as schema from "./schema";
 import mysql from "mysql2/promise";
 
-const connection = await mysql.createConnection({
+const poolConnection = mysql.createPool({
   host: process.env.MYSQL_HOST || "localhost",
   user: process.env.MYSQL_USER || "root",
   password: process.env.MYSQL_PASSWORD || "password",
   database: process.env.MYSQL_DATABASE || "db",
 });
 
-export const db = drizzle(connection, { schema, mode: "default" });
+export const db = drizzle(poolConnection, { schema, mode: "default" });
